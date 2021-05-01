1749338531d51d58ae13a8aad072801adaf48ef3
yongsk0066 <yongsk0066@naver.com>
2021-05-01 16:56:15 +0900

    initial commit

---
+++ src/SidebarProvider.ts
@@ -0,0 +1,63 @@
+import * as vscode from "vscode";
+
+export class SidebarProvider implements vscode.WebviewViewProvider {
+    public static readonly viewType = "dgu-sidebar";
+
+    private _view?: vscode.WebviewView;
+
+    constructor(private readonly _extensionUri: vscode.Uri) {}
+
+    public resolveWebviewView(webviewView: vscode.WebviewView) {
+        this._view = webviewView;
+        webviewView.webview.options = {
+            // Allow scripts in the webview
+            enableScripts: true,
+
+            localResourceRoots: [this._extensionUri],
+        };
+        webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+    }
+
+    private _getHtmlForWebview(webview: vscode.Webview) {
+        const scriptUri = webview.asWebviewUri(
+            vscode.Uri.joinPath(this._extensionUri, "media", "main.js")
+        );
+
+        const styleResetUri = webview.asWebviewUri(
+            vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
+        );
+        const styleVSCodeUri = webview.asWebviewUri(
+            vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
+        );
+        const styleMainUri = webview.asWebviewUri(
+            vscode.Uri.joinPath(this._extensionUri, "media", "main.css")
+        );
+
+        return `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+
+            <!--
+                Use a content security policy to only allow loading images from https or from our extension directory,
+                and only allow scripts that have a specific nonce.
+            -->
+            <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource};">
+
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+            <link href="${styleResetUri}" rel="stylesheet">
+            <link href="${styleVSCodeUri}" rel="stylesheet">
+            <link href="${styleMainUri}" rel="stylesheet">
+            
+            <title>Case Generator</title>
+        </head>
+        <body>
+            <input/>
+            <button class="add-color-button">Generate</button>
+
+            <script src="${scriptUri}"></script>
+        </body>
+        </html>`;
+    }
+}
